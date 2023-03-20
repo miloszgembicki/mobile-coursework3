e8dc2ac1ee8dfb1889def2eee368d999f62d9296
Milosz Gembicki <mg1313@live.mdx.ac.uk>
2023-03-20 02:22:29 +0000

    Edited the main.js file!

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faShoppingCart, faSave, faTrash, faSync, faTextHeight } from '@fortawesome/free-solid-svg-icons'
+import { faUniregistry } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faShoppingCart, faTextHeight, faUniregistry, faSave, faTrash, faSync)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
   render: (h) => h(App)
