de1a53ae1532dce174f1ba421cad1fae0e7b11a8
Milosz Gembicki <mg1313@live.mdx.ac.uk>
2023-03-20 02:42:44 +0000

    commiting dist

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+var cacheName = "classes-v1";
+var cacheFiles = [
+    "index.html",
+    // "subjects.j",
+    "images/arts.png",
+    "images/english.png",
+    "images/history.png",
+    "images/icon-192.png",
+    "images/icon-512.png",
+    "images/it.png",
+    "images/math.png",
+    "images/music.png",
+    "images/science.png"
+];
+self.addEventListener("install", function (e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            //return the file if it is not in the cache
+            if (cachedFile) {
+                console.log(
+                    "[Service Worker] Resource fetched from the cache for: " +
+                    e.request.url
+                );
+                return cachedFile;
+            } else {
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        //add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log(
+                            "[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url
+                        );
+                        return response;
+                    });
+                });
+            }
+        })
+    );
+});
